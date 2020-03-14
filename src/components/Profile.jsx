8fa778ff99d1914eb2069e57e6fb3b34842939b5
Camatoz <yurabondik99@gmail.com>
2020-03-14 20:20:41 +0200

    Start Project

---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+    return(
+        <div className="content">
+            <div>
+                <img src="https://cdn.pixabay.com/photo/2015/02/24/15/41/dog-647528_960_720.jpg"/>
+            </div>
+            <div>
+                ava
+            </div>
+            <div>
+                post
+                <div>
+                new post
+                </div>
+            </div>
+            <div>
+                <div>1</div>
+                <div>2</div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
