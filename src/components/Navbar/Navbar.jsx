0647f63acfd7445f0ba8e4a81823d28bc1a6e00d
Camatoz <yurabondik99@gmail.com>
2020-04-27 21:03:53 +0300

    add class Active

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import test from './Navbar.module.css';
+import { NavLink } from 'react-router-dom';
 console.log(test);
 
 
 const Navbar = () => {
     return(
         <nav className={test.nav}>
-            <div className={`${test.item} ${test.active}`}><a href="/profile">Profile</a></div>
-            <div className={test.item}><a href="/dialogs">Messages</a></div>
-            <div className={test.item}><a href="/news">News</a></div>
-            <div className={test.item}><a href="/music">Music</a></div>
-            <div className={test.item}><a href="/tuning">Settings</a></div>
+            <div className={test.item}><NavLink to="/profile" activeClassName={test.active}>Profile</NavLink></div>
+            <div className={test.item}><NavLink to="/dialogs" activeClassName={test.active}>Messages</NavLink></div>
+            <div className={test.item}><NavLink to="/news" activeClassName={test.active}>News</NavLink></div>
+            <div className={test.item}><NavLink to="/music" activeClassName={test.active}>Music</NavLink></div>
+            <div className={test.item}><NavLink to="/tuning" activeClassName={test.active}>Settings</NavLink></div>
         </nav>
     )
 }
